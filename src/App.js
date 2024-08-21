c2406c384fdd9f0634928d0005ea86fda6008198
Jaeeun Kim <dmjfhe12@gmail.com>
2024-08-21 17:48:50 +0100

    Router setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+
+// 1. 전체상품페이지, 로그인, 상품상세페이지
+// 2. 전체상품페이지에서는 전체 상품을 볼 수 있다.
+// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다.
+// 4. 상품 디테일을 눌렀으나, 로그인이 안됬을 경우에는 로그인페이지가 먼저 나온다.
+// 5. 로그인이 되어있을 경우에는 상품 디테일 페이지를 볼 수 있다.
+// 6. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
+// 7. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다, 다시 로그인페이지가 보인다.
+// 8. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<ProductAll></ProductAll>} />
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/product/:id" element={<ProductDetail></ProductDetail>} />
+      </Routes>
     </div>
   );
 }
