51ce6881e46a73e7036b86f88a072d1e342fc193
Jaeeun Kim <dmjfhe12@gmail.com>
2024-08-23 17:52:18 +0100

    detail page

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 const ProductDetail = () => {
-  return <div>상품 상세 페이지</div>;
+  let { id } = useParams();
+  const [product, setProduct] = useState();
+  const getProductDetail = async () => {
+    let url = `http://localhost:5000/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProduct(data);
+  };
+
+  useEffect(() => {}, []);
+  getProductDetail();
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className="product-img">
+            <img src={product?.img} />
+          </Col>
+          <Col>
+            <div>{product?.title}</div>
+            <div>{product?.price}</div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductDetail;
