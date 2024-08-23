5a9f2e1f60d93d12128623af5b99b584afc0169b
Jaeeun Kim <dmjfhe12@gmail.com>
2024-08-23 18:08:14 +0100

    json server

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -8,7 +8,7 @@ const ProductDetail = () => {
   let { id } = useParams();
   const [product, setProduct] = useState();
   const getProductDetail = async () => {
-    let url = `http://localhost:5000/products/${id}`;
+    let url = `https://my-json-server.typicode.com/kimjaeeungit/react-hnm/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
