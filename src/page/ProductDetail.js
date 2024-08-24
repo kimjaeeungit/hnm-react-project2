ff0479a2a34ffcc0e28092a1867fc24ba99a67c7
Jaeeun Kim <dmjfhe12@gmail.com>
2024-08-24 21:37:26 +0100

    modify detail page

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { Row, Col } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 const ProductDetail = () => {
   let { id } = useParams();
@@ -11,7 +12,7 @@ const ProductDetail = () => {
     let url = `https://my-json-server.typicode.com/kimjaeeungit/react-hnm/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
+    // console.log(data);
     setProduct(data);
   };
 
@@ -27,7 +28,16 @@ const ProductDetail = () => {
           <Col>
             <div>{product?.title}</div>
             <div>{product?.price}</div>
+            <div>{product?.choice == true ? 'Conscious choice' : ''}</div>
+            <Form.Select size="sm">
+              <option>사이즈 선택</option>
+              <option value="1">S</option>
+              <option value="2">M</option>
+              <option value="3">L</option>
+            </Form.Select>
+            <Button variant="dark">추가</Button>
           </Col>
+          <Col></Col>
         </Row>
       </Container>
     </div>
